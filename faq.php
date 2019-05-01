f7dc6339b1d562361f56794a0a5a3c7bf9ffd887
Jessica <jessica.galvan@hotmail.com>
2019-04-30 23:17:20 -0300

    Fix FAQ y cree array de preguntas

--- faq.php
+++ faq.php
@@ -32,149 +32,28 @@
           <div class="faq-body">
             <!--Titulo-->
             <h1 class="faq-title">Preguntas Frecuentes</h1>
+            <?php include("includes/lista-de-faq.php");?>
 
             <!--Seccion de Preguntas-->
             <div class="ayuda">
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Dónde puedo encontrar sus productos?</h2>
-                 </div>
-
-<<<<<<< HEAD
-               <div class="respuestas">
-                 <p>Actualmente nuestros productos se consiguen solo de forma online en nuestra página oficial.</p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Su marca testea en animales?</h2>
-               </div>
-               <div class="respuestas">
-                 <p>No, estamos orgullosos de decir que desde el inicio nunca testeamos  ninguno de nuestros productos en animales. </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Sus productos son libres de gluten?</h2>
-               </div>
-               <div class="respuestas">
-                 <p>Sí, es importante para nosotros cuidar de la salud de nuestras clientas y sabemos que muchas tienen alergias relacionadas a este por lo que todos nuestros productos son libres de gluten. </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Hacen envíos internacionales?</h2>
-               </div>
-               <div class="respuestas">
-                 <p>No, por el momento hacemos envíos dentro de Argentina. </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Dónde fabrican y crean sus productos?</h2>
-               </div>
-               <div class="respuestas">
-                 <p>Se diseñan y fabrican dentro de Argentina. </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Cómo hago para hacer el seguimiento de mi orden de compras? </h2>
-               </div>
-               <div class="respuestas">
-                 <p>En la página oficial de Correo Argentino vas a poder hacer el seguimiento con tu orden de compra. </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Qué hago si mi orden llega con productos rotos?</h2>
-               </div>
-               <div class="respuestas">
-                 <p> ¡Escribinos a nuestro mail y te daremos una solucion! </p>
-               </div>
-             </section>
-           <section class="cajaDePregunta">
-               <div class="preguntas">
-                 <h2>¿Puedo comprar sombras individuales específicas que vengan en sus paletas de ojos?</h2>
-               </div>
-               <div class="respuestas">
-                 <p>No, por el momento las sombras que se encuentran dentro de las paletas de ojos no se venden de forma individual. </p>
-               </div>
-             </section>
-         </div>
-        </div>
-      </main>
-=======
-                 <div class="respuestas">
-                   <p>Actualmente nuestros productos se consiguen solo de forma online en nuestra página oficial.</p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Su marca testea en animales?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>No, estamos orgullosos de decir que desde el inicio nunca testeamos  ninguno de nuestros productos en animales. </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Sus productos son libres de gluten?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>Sí, es importante para nosotros cuidar de la salud de nuestras clientas y sabemos que muchas tienen alergias relacionadas a este por lo que todos nuestros productos son libres de gluten. </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Hacen envíos internacionales?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>No, por el momento hacemos envíos dentro de Argentina. </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Dónde fabrican y crean sus productos?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>Se diseñan y fabrican dentro de Argentina. </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Cómo hago para hacer el seguimiento de mi orden de compras? </h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>En la página oficial de Correo Argentino vas a poder hacer el seguimiento con tu orden de compra. </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Qué hago si mi orden llega con productos rotos?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p> Escribinos a nuestro mail y te daremos una solucion! </p>
-                 </div>
-               </section>
-             <section class="cajaDePregunta">
-                 <div class="preguntas">
-                   <h2>¿Puedo comprar sombras individuales específicas que vengan en sus paletas de ojos?</h2>
-                 </div>
-                 <div class="respuestas">
-                   <p>No, por el momento las sombras que se encuentran dentro de las paletas de ojos no se venden de forma individual. </p>
-                 </div>
-               </section>
-           </div>
-          </div>
+              <?php for ($i=0; $i < count($listaDePreguntas); $i++): ?>
+              <section class="cajaDePregunta">
+                <div class="preguntas">
+                  <h2><?=$listaDePreguntas[$i]["pregunta"];?></h2>
+                </div>
+                <div class="respuestas">
+                  <p><?=$listaDePreguntas[$i]["respuesta"];?></p>
+                </div>
+              </section>
+              <?php endfor; ?>
+            </div> <!--end Seccion Preguntas -->
+          </div>  <!-- end Contenedor de Preguntas-->
         </main>
->>>>>>> master
-
-         <!--FOOTER-->
-         <?php
-         include_once("includes/footer.php");
-         ?>
-       </div>  <!--FIN BODY-CONTAINER-->
-      </div> <!--FIN XL-SCREEN-->
+       <!--FOOTER-->
+       <?php
+       include_once("includes/footer.php");
+       ?>
+      </div>  <!--FIN BODY-CONTAINER-->
+    </div> <!--FIN XL-SCREEN-->
    </body>
  </html>

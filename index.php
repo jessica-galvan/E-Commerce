0d9b75c93e0e2db8c0dc160e76fcfca063650663
Jessica <jessica.galvan@hotmail.com>
2019-05-02 23:20:49 -0300

    Sprint 1 - versionFinal
    Agregamos etiqueta "New"

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="css/master.css">
     <link rel="stylesheet" href="css/index.css">
@@ -91,6 +92,8 @@
             while($maxProductosNuevos < 5) {
               if ($productos[$nNuevos]["estado"] === "Nuevo"):?>
                 <article class="producto">
+                  <img class="etiqueta-nuevo" src="img/new/NewVioleta.png" alt="">
+
                   <div class="p-imagen">
                     <img src="img/productos/<?=$productos[$nNuevos]["foto"]?>" alt="<?=$productos[$nNuevos]["nombre"]?>">
                   </div>

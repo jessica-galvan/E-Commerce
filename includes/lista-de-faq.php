f99fc97e63b24e5cd30db584db6b7c531ed75b99
Anto <antocardone14@gmail.com>
2019-05-09 17:54:38 -0300

    Arreglo de preguntas repetidas
    Varias preguntas del FAQ aparecian mas de una vez

--- includes/lista-de-faq.php
+++ includes/lista-de-faq.php
@@ -32,34 +32,7 @@
       "pregunta" => "¿Puedo comprar sombras individuales específicas que vengan en sus paletas de ojos?",
       "respuesta" => "No, por el momento las sombras que se encuentran dentro de las paletas de ojos no se venden de forma individual. <br> Actualmente nuestros productos se consiguen solo de forma online en nuestra página oficial."
     ],
-    [
-      "pregunta" => "¿Su marca testea en animales?",
-      "respuesta" => "No, estamos orgullosos de decir que desde el inicio nunca testeamos ninguno de nuestros productos en animales."
-    ],
-    [
-      "pregunta" => "¿Sus productos son libres de gluten?",
-      "respuesta" => "Sí, es importante para nosotros cuidar de la salud de nuestras clientas y sabemos que muchas tienen alergias relacionadas a este por lo que todos nuestros productos son libres de gluten."
-    ],
-    [
-      "pregunta" => "¿Hacen envíos internacionales?",
-      "respuesta" => "No, por el momento hacemos envíos dentro de Argentina."
-    ],
-    [
-      "pregunta" => "¿Dónde fabrican y crean sus productos?",
-      "respuesta" => "Se diseñan y fabrican dentro de Argentina."
-    ],
-    [
-      "pregunta" => "¿Cómo hago para hacer el seguimiento de mi orden de compras?",
-      "respuesta" => "En la página oficial de Correo Argentino vas a poder hacer el seguimiento con tu orden de compra."
-    ],
-    [
-      "pregunta" => "¿Qué hago si mi orden llega con productos rotos?",
-      "respuesta" => "Escribinos a nuestro mail y te daremos una solucion!"
-    ],
-    [
-      "pregunta" => "¿Puedo comprar sombras individuales específicas que vengan en sus paletas de ojos?",
-      "respuesta" => "No, por el momento las sombras que se encuentran dentro de las paletas de ojos no se venden de forma individual."
-    ]
+   
   ]; //Cierra el array.
 
 
